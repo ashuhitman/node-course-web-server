765961eca5e17e8d6ddaad57cffd2fc546cf49f6
Ashu <ashu8306@gmail.com>
2018-06-28 07:19:12 +0530

    setup startr script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+console.log(process.env.PORT);
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -37,4 +40,6 @@ app.get('/about',(req, res) =>{
    });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+   console.log(`server is up on port ${port}`);
+});
